ebb79f7bf6a9c8e283200dbe5e5f72e2e608b888
Edvan Ribeiro <jk.jr47@gmail.com>
2024-03-22 21:42:59 -0300

    created header and main

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         sans: ["Roboto Flex", "sans-serif"],
         company: ["Bungee", "sans-serif"],
       },
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(258deg, rgba(2,0,36,0.01) 0%, rgba(13,16,26,0.57) 52%, rgba(22,28,45,1) 100%), url('../images/hero.png')",
+      },
+      colors: {
+        sambucus: "#161C2D",
+      },
     },
   },
   plugins: [],
